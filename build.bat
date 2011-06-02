c0f0e43801d267542fe3cd939ecaedfa385402cd
qiushilee <showbei@gmail.com>
2011-06-02 10:23:55 +0800

    update comments&echo.

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
-:: 构建css
+:: Package css
 echo ----------------------
-echo 正在构建css...
+echo Pckage css...
 echo ----------------------
 call ant -buildfile css\build.xml
 
-:: 获取当前目录名
+:: Get current dirname
 set "cd_=%cd%"
 :loop
 set "cd_=%cd_:*\=%"
@@ -13,20 +13,20 @@ set "cd_tmp=%cd_:\=%"
 if not "%cd_tmp%"=="%cd_%" goto loop
 mkdir "build\compile\zip\"%cd_%
 
-:: 构建模板页
+:: Compile templates
 echo ----------------------
-echo 正在构建模板页...
+echo Compile templates...
 echo ----------------------
 call ant -buildfile build\build.xml -Dbuild.dir.zip=compile\zip\%cd_% -Dzip.name=%cd_%
 
-:: 上传压缩包
+:: Release package
 echo ----------------------
-echo 正在上传压缩包...
+echo Release package...
 echo ----------------------
 call ant -buildfile build\build.xml fnt.release -Dbuild.dir.zip=compile\zip\%cd_%
 
-:: 清理文件
+:: Clear file
 echo ----------------------
-echo 正在清理文件...
+echo Clear file...
 echo ----------------------
 call ant -buildfile build\build.xml fnt.clear -Dbuild.dir.zip=compile\zip\%cd_%
